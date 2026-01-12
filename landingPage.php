32461557aede939e4395e7c71151e0c0a339aa8a
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-01-12 11:58:10 +0000

    Start to landing page

---
+++ landingPage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inquizitive - educational quizzes</title>
+</head>
+
+<body>
+    <div id="header">Header</div>
+    
+    <div id="flex-row">Header</div>
+</body>
+</html>
