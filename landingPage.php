cb90167f7e2879a3cb8c43dbec735a0fa9a295ff
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-01-14 16:38:27 +0000

    Set up at home, starting to build page and css structure

--- landingPage.php
+++ landingPage.php
@@ -5,11 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inquizitive - educational quizzes</title>
+    <link rel="stylesheet" href="landingPage.css">
+    <link rel="stylesheet" href="colours.css">
+    <link rel="stylesheet" href="global.css">
 </head>
 
 <body>
     <div id="header">Header</div>
     
-    <div id="flex-row">Header</div>
+    <div class="flex-row landing-page-row halves-row 100w">Row</div>
+    
+    <div class="flex-row landing-page-row halves-row 100w">Row</div>
+    
+    <div class="flex-row landing-page-row halves-row 100w">Row</div>
 </body>
 </html>
