ec4e9f8f6c8d701f9814c4ceeaf95ae555ad9a46
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-02-03 20:58:41 +0000

    Landing page structure with placeholders, testing styles and minor bugs
    Positioning for major elements, particularly containers, with an overall page structure. Using Inquizitive branded colours.
    
    Missing Quicksand font, has some minor positioning and sizing problems, text and pictures is all placeholder.
    
    Email form hasn't been started yet.

---
+++ contents/landingPage.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inquizitive - educational quizzes</title>
+    <link rel="stylesheet" href="../css/landingPage.css">
+    <link rel="stylesheet" href="../css/colours.css">
+    <link rel="stylesheet" href="../css/global.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Mogra&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Pacifico&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="header">
+        <div>Inquizitive Logo</div>
+        <div>
+            <button class="button">Login</button>
+            <button class="bold-button">Create an account</button>
+        </div>
+    </div>
+    
+    <!-- First row - welcome to Inquizitive -->
+    <div id="first-row" class="flex-row lp-row halves-row 100w">
+        <div class="spacing-margin">
+            <h1>Welcome to Inquizitive!</h1>
+            <p>UCW's own reinforcement testing platform.</p>
+            <button>Find out more</button>
+        </div>
+        <div>
+            <img class="spacing-margin" src="../images/sampleimage.png" alt="sample">
+        </div>
+    </div>
+    
+    <!-- Second row -->
+    <div id="second-row" class="flex-row lp-row halves-row 100w">
+        <div>
+            <img class="spacing-margin" src="../images/sampleimage.png" alt="sample">
+        </div>
+        <div class="spacing-margin">Text portion</div>
+    </div>
+    
+    <!-- Third row - how to use Inquizitive -->
+    <div id="guide-row" class="flex-row lp-row thirds-row 100vw">
+        <div class="flex-col">
+            <img src="../images/sampleimage.png" alt="sample">
+            <div class="textbox">
+                <h2>First Step</h2>
+                <p>First step things</p>
+            </div>
+            
+        </div>
+        <div class="flex-col">
+            <img src="images/sampleimage.png" alt="sample">
+            <div class="textbox">
+                <h2> Step</h2>
+                <p> step things</p>
+            </div>
+        </div>
+        <div class="flex-col">
+            <img src="images/sampleimage.png" alt="sample">
+            <div class="textbox">
+                <h2> Step</h2>
+                <p> step things</p>
+            </div>
+        </div>
+    </div>
+    
+    <div id="bottom-row">Row</div>
+</body>
+</html>
