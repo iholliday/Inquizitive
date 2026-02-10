ab2b9598e2098577a24b1a77674fae0081f601e8
ahahahaha1 <tomprivateemail247@gmail.com>
2026-02-10 22:28:09 +0000

    Updated login page based on feedback
    - Changed class names to better fit naming conventions
    - Added box shadow to form
    - Added gradient to background
    - Created divider and centred signup button
    - Changed login-help text colour, hovering now displays accent colour.
    - Added page title.

--- contents/login.php
+++ contents/login.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="./css/colours.css" />
     <link rel="stylesheet" href="./css/login.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login | Inquizitive </title>
 
     <!-- Required scripts -->
     <script
@@ -28,28 +29,30 @@
     ?>
 
     <!-- Login Page -->
-    <body class="loginPage">
+    <body class="login-page">
         <main>
-            <div class="loginBackground">
-                <div class="container loginContainer p-4 mt-4" data-bs-theme="dark">
+            <div class="login-background">
+                <div class="container login-container p-4 mt-4" data-bs-theme="dark">
                     <div class="grid-container">
                         <div class="grid-item item1">
                             <img src="contents/images/login.png" alt="Login image">
                         </div>
                         <div class="grid-item item2">
-                            <h2 class="loginTitle">Sign in</h2>
-                            <form method="POST" id="login-form" class="loginForm">
+                            <h2 class="login-title">Sign in</h2>
+                            <form method="POST" id="login-form">
                                 <div class="mb-3">
-                                    <input type="text" name="txtEmail" class="form-control loginFormControl" id="email" placeholder="Email Address" required>
+                                    <input type="text" name="txtEmail" class="form-control login-form-control" id="email" placeholder="Email Address" required>
                                 </div>
                                 <div class="mb-3">
-                                    <input type="password" name="txtPass" id="password" class="form-control loginFormControl" placeholder="Password" aria-describedby="passwordHelpBlock" required>
+                                    <input type="password" name="txtPass" id="password" class="form-control login-form-control" placeholder="Password" aria-describedby="passwordHelpBlock" required>
                                 </div>
-                                <p class="login-help" id="forgot-password"><a href="/">Forgot password?<a></p>
+                                <p class="login-help" id="forgot-password"><a href="/">Forgot password?</a></p>
                                 <div class="g-recaptcha" data-sitekey="PLACEHOLDER" data-callback="enableSubmitBtn"></div>
                                 <!-- <div class="g-recaptcha" data-sitekey="<?= $_ENV['RECAPTCHA_SITE_KEY'] ?>" data-callback="enableSubmitBtn"></div>-->
                                 <button type="submit" id="submitBtn" class="btn btn-danger btn-signin" >Sign in</button> <!-- add disabled -->
-                                <p class="login-help" id="sign-up-help"><a href="/">Don't have an account?</a></p>
+                                <div class="signup-divider">
+                                    <p class="login-help" id="sign-up-help"><a href="/">Don't have an account?</a></p>
+                                </div>
                         </div>
                     </div>
                 </div>
