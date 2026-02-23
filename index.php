7ab6d05d363a765061bae3b91eac32e2a4abfac5
ahahahaha1 <tomprivateemail247@gmail.com>
2026-02-23 17:27:42 +0000

    Fixed loadPage vulnerability & updated CSS
    - Wrapped loadPage inside $(document).ready(), ensuring function is only accessible after the DOM is ready.
    - Updated signup page styling and fixed small mobile adaptation bug.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     // Routes required for all pages.
     $route->Route(['post'], '/auth', "./php/auth.php");
     $route->Route(['post'], '/createAccount', "./php/createAccount.php");
-    $route->Route(['get'], '/dashboard', "./contents/dashboardNavigation.php",["ADMIN"]);
+    $route->Route(['get'], '/dashboard', "./contents/dashboardNavigation.php");
     $route->Route(['get'], '/subjectsPage', "./contents/includes/inc-SubjectsDashboardPage.php");
     $route->Route(['get'], '/tempCreateUser', "./contents/tempCreateUser.php");
     $route->Route(['get'], '/signup', "./contents/signup.php");
--- js/loginSignupNav.js
+++ js/loginSignupNav.js
@@ -1,28 +1,30 @@
-// Ajax for loading full pages prior to signin.
-function loadPage(page)
+$(document).ready(function()
 {
-    $.ajax({
-        url: "./" + page, 
-        type: 'GET',
-        success: function(data) {
-            // If successful, load html.
-            $('body').html(data);
-        },
-        error: function() {
-            // Else display error.
-            alert("Error loading dashboard. Please try again.");
-        }
+    // Ajax for loading full pages prior to signin.
+    function loadPage(page)
+    {
+        $.ajax({
+            url: "./" + page, 
+            type: 'GET',
+            success: function(data) {
+                // If successful, load html.
+                $('body').html(data);
+            },
+            error: function() {
+                // Else display error.
+                alert("Error loading dashboard. Please try again.");
+            }
+        });
+    }
+
+    $("#loginLink").click(function(e){
+        e.preventDefault();
+        loadPage("login");
     });
-}
 
-$(document).on("click", "#loginLink", function(e) {
-    e.preventDefault();
-    loadPage("login");
+    $("#signupLink").click(function(e){
+        e.preventDefault();
+        loadPage("signup");
+    });
 });
-
-$(document).on("click", "#signupLink", function(e) {
-    e.preventDefault();
-    loadPage("signup");
-});
-
 
