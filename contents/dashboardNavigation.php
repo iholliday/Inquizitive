fded78b3ccb63dc59ff6939c0ae005ffa8f87c9e
ahahahaha1 <tomprivateemail247@gmail.com>
2026-02-26 18:09:03 +0000

    Removed unneeded script.
    - Removed loginSignupNav.js from dashboardNavigation file.

--- contents/dashboardNavigation.php
+++ contents/dashboardNavigation.php
@@ -13,8 +13,6 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 </head>
 
-<script src="./js/loginSignupNav.js"></script>
-
 <body>
     <div id ="h" tabindex="0" class="click hidden"></div>
     <div class="app">
