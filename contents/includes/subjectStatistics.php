bbb6fad3ebeb590da205b72e9a8640c344b1bb9f
FluffyWerewolfie <160604994+FluffyWerewolfie@users.noreply.github.com>
2026-03-10 16:49:50 +0000

    Push because bug wasn't showing

--- contents/includes/subjectStatistics.php
+++ contents/includes/subjectStatistics.php
@@ -6,13 +6,14 @@ header('Content-type: application/json');
 $db = new inquizitiveDB();
 $userUUID = $_SESSION['userUUID'];
 $subjectUUID = $_POST['subjectUUID'];
-$result = $db-Query("CALL GetTotalScorePerSubjectByUserUUID(?,?)" , [$userUUID,$subjectUUID]);
+$result = $db->Query("CALL GetTotalScorePerSubjectByUserUUID(?,?)" , [$userUUID,$subjectUUID]);
 $dataArray = [['Price','Size']];
 if(mysqli_num_rows($result) > 0 )
 {
+    $arr =
     array_push($dataArray,[$row['score'],$row['completionDate']]);
 }
-
+  
 echo json_encode([['Price', 'Size'],
   [50,7],[60,8],[70,8],[80,9],[90,9],
   [100,9],[110,10],[120,11],
