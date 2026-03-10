f08f89b02f42a1008f479ceb7e9808c228500a91
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-03-10 15:46:12 +0000

    Button added

--- contents/includes/subjectsPage.php
+++ contents/includes/subjectsPage.php
@@ -9,6 +9,17 @@
     require __DIR__ . '/../dashboardNavigation.php';
     exit;
     }
+
+    $userID = $_SESSION["userUUID"];
+    $db = New inquizitiveDB();
+    $result = $db->Query("CALL GetUserSubjectsByUUID(?)", [$userID]);
+    $dataArray = [];
+
+    if (mysqli_num_rows($result) > 0) {
+        while (
+            $row = mysqli_fetch_assoc($result)
+        ){array_push($dataArray, $row);}
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +28,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/subjects.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=equalizer" />
+
     <title>Subjects Page</title>
 </head>
 <body>
@@ -26,8 +40,37 @@
 
             <div class="flex-row subject-row" style="min-height: 1000px">
                 <div class="flex-col flex-grow-2">
-                    <div class="card">How many Subjects</div>
-                    <div class="card height-100">Table of Subjects</div>
+                    <div class="card">How many Subjects
+                        <div><?php echo htmlspecialchars(sizeof($dataArray)) ?></div>
+                    </div>
+                    <div class="card height-100">Table of Subjects
+                        <div>
+                            <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Author</th>
+                                <th scope="col">View Stats</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            foreach ($dataArray as $item) {
+                                ?><tr>
+                                    <th scope="row"><?=htmlspecialchars($item["subjectTitle"]) ?></th>
+                                    <td><?=htmlspecialchars($item["subjectDescription"]) ?></td>
+                                    <td><?=htmlspecialchars($item["authorName"]) ?></td>
+                                    <td><button data="<?=htmlspecialchars($item["authorName"]) ?>"><i class="material-symbols-outlined">
+equalizer
+</i></button></td>
+                                </tr><?php
+                            }
+                            ?>
+                            </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
                 <div class="flex-col flex-grow-1">
                     <div class="card height-100">Statistics</div>
