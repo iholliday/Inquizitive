91b342475f0ff92a67f58696e979c5b74cc6e55f
FluffyWerewolfie <160604994+FluffyWerewolfie@users.noreply.github.com>
2026-03-11 12:18:23 +0000

    Statistics functionality for the subjects page
    Users can now see how well they are performing in terms of their score over time for all of their subjects!

--- contents/includes/subjectStatistics.php
+++ contents/includes/subjectStatistics.php
@@ -7,12 +7,20 @@ $db = new inquizitiveDB();
 $userUUID = $_SESSION['userUUID'];
 $subjectUUID = $_POST['subjectUUID'];
 $result = $db->Query("CALL GetTotalScorePerSubjectByUserUUID(?,?)" , [$userUUID,$subjectUUID]);
-$dataArray = [['Date','Price']];
+$dataArray = [['Date','Score']];
 
 if(mysqli_num_rows($result) > 0 )
 {
-    $arr =[$row['completionDate'],$row['score']];
-    array_push($dataArray,$arr);
+    while($row= mysqli_fetch_assoc($result))
+    {
+        if($row['totalScore'] !== NULL)
+        {
+            $arr =[htmlspecialchars($row['completionDate']),(int)htmlspecialchars($row['totalScore'])];
+            array_push($dataArray,$arr);
+        }
+
+    }
+
 }else
 {
     $arr =[date("h:i:s"),0];
