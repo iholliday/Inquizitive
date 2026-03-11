abaf650f8d415392c226a69636e5aa8c2721d40f
FluffyWerewolfie <160604994+FluffyWerewolfie@users.noreply.github.com>
2026-03-11 10:41:07 +0000

    Inventory now allows users to change their profile picture
    Removed gacha page, users now can no longer experience the joys of the surprise mechanics based on client request

--- contents/includes/inventoryItemsPage.php
+++ contents/includes/inventoryItemsPage.php
@@ -29,6 +29,7 @@ $shop->sortRarities();
 ?>
 
                 <div class="shop-items shop-common">
+                    <div class="shop-items-header">Here you can click an owned item to select as your main profile picture</div>
                     <div class="shop-items-header">Common</div>
                     <div class="shop-items-header-divider"></div>
                     <div class="shop-items-grid">
@@ -38,7 +39,7 @@ $shop->sortRarities();
                             for($i=0; $i<sizeof($shop->getCommonItems());$i++)
                             {
                                 echo '
-                                <div class="shop-item">
+                                <div class="shop-item" item-src="' . $shop->getCommonItems()[$i]->getSprite() . '">
                                     <img src="' . $shop->getCommonItems()[$i]->getSprite() . '" /> 
                                     <div class="shop-item-price" item-id="' . $shop->getCommonItems()[$i]->getUUID() . '">' . $shop->getCommonItems()[$i]->getQuantity() . '</div>
                                 </div>';
@@ -60,7 +61,7 @@ $shop->sortRarities();
                             for($i=0; $i<sizeof($shop->getEpicItems());$i++)
                             {
                                 echo '
-                                <div class="shop-item">
+                                <div class="shop-item" item-src="' . $shop->getEpicItems()[$i]->getSprite() . '">
                                     <img src="' . $shop->getEpicItems()[$i]->getSprite() . '" /> 
                                     <div class="shop-item-price" item-id="' . $shop->getEpicItems()[$i]->getUUID() . '">' . $shop->getEpicItems()[$i]->getQuantity() . '</div>
                                 </div>';
@@ -82,15 +83,57 @@ $shop->sortRarities();
                             for($i=0; $i<sizeof($shop->getLegendaryItems());$i++)
                             {
                                 echo '
-                                <div class="shop-item">
+                                <div class="shop-item" item-src="' . $shop->getLegendaryItems()[$i]->getSprite() . '">
                                     <img src="' . $shop->getLegendaryItems()[$i]->getSprite() . '" /> 
                                     <div class="shop-item-price" item-id="' . $shop->getLegendaryItems()[$i]->getUUID() . '">' . $shop->getLegendaryItems()[$i]->getQuantity() . '</div>
                                 </div>';
                             }
                         }else
                         {
-                            echo'<div class="shop-no-item">No items at this rarity in inventorye</div>';
+                            echo'<div class="shop-no-item">No items at this rarity in inventory</div>';
                         }
                         ?>
                     </div>
                 </div>
+<script>
+    profilePicture="";
+$(".shop-item").ready(function(){
+    $(".shop-item").click(function(){
+        profilePicture = $(this).attr("item-src").toString();
+        $.ajax({
+                    url: './updateAvatar', 
+                    type: 'POST',
+                    data: {
+                        avatar: profilePicture,
+                    }, 
+                    success: function(response)
+                    {
+                        console.log(response);
+                        if(response.status == "success")
+                        {
+                            
+                            Swal.fire({
+                            icon: "success",
+                            title: response.message,
+                            showConfirmButton: false,
+                            timer: 1500
+                            });
+                            $(".avatar").attr("src",profilePicture);
+                        }else if(response.status == "error")
+                        {
+                            Swal.fire({
+                            icon: "error",
+                            title: response.message,
+                            showConfirmButton: false,
+                            timer: 1500
+                            });
+                        }
+                    },
+                    error: function(e)
+                    {
+                        console.log(e);
+                    }
+        })
+    })
+})
+</script>
--- index.php
+++ index.php
@@ -79,6 +79,7 @@
     $route->Route(['post'], '/editLecturer', "./php/editLecturer.php");
     $route->Route(['post'], '/edit-lecturer', "./contents/includes/editLecturer.php");
     $route->Route(['post'], '/getLeaderboard', "./php/getLeaderboard.php");
+    $route->Route(['post'], '/updateAvatar', "./php/updateAvatar.php");
 
     $route->Route(['get'], '/last-page', "./contents/includes/pageSession.php");
     $route->Route(['get'], '/logout', "./php/logout.php");
